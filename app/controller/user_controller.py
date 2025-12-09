e8cf3766a1edd622d07f87da5e61176c5db3763a
Md. Tazwarul Islam Abir <mdabir@joinventureai.com>
2025-12-09 11:13:29 +0600

    add bg task setup in create user for sending email

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter,Depends,HTTPException,BackgroundTasks
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.schemas.user_schema import CreateUserWithProfile ,UserRead
 from app.core.database import get_session
@@ -8,8 +8,8 @@ router=APIRouter(prefix="/users",tags=["users"])
 
 @router.post('/signup', response_model=UserRead)
 async def create_user(
-    data:CreateUserWithProfile,db:AsyncSession=Depends(get_session)
+    data:CreateUserWithProfile,  background_tasks: BackgroundTasks,db:AsyncSession=Depends(get_session),
 ):
-        user = await UserService.create_user_with_profile(db, data)
+        user = await UserService.create_user_with_profile(db, data,background_tasks)
         return user
    
--- app/service/user_service.py
+++ app/service/user_service.py
@@ -6,10 +6,12 @@ from app.models.user_model import User
 from app.models.user_profile_model import UserProfile
 from app.utils.generate_expire_time import gen_exp_time
 from app.utils.service.send_email import send_email
+from fastapi import BackgroundTasks
+
 class UserService:
     @staticmethod
     async def create_user_with_profile(
-        db:AsyncSession,data:CreateUserWithProfile
+        db:AsyncSession,data:CreateUserWithProfile, background_tasks: BackgroundTasks
     ):
       async with db.begin():     
         user=User(email=data.email,password=data.password)
@@ -23,5 +25,14 @@ class UserService:
         authentication=await UserRepository.craete_new_authentication(db,authentication)
         
         db.refresh(user)
-        await send_email("md.tazwarul.islam.07@gmail.com","tesing email", "test message")
+
+     
+        # Schedule email in the background
+        background_tasks.add_task(
+                send_email,
+                "md.tazwarul.islam.07@gmail.com",
+                "Testing email",
+                "Test message"
+            )
+        
         return {"user_id":user.id,"message":"A verification code has been sent to your email."}
